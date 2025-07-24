8b042c8309797606005812c0376b694f85742ac8
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-07-24 08:18:14 +0530

    new

---
+++ insertdata.js
@@ -0,0 +1,24 @@
+const mysql  = require('mysql')
+const connection  = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'node'
+})
+connection.connect((err)=>{
+    if(err)
+    {
+        console.error('connection error',err.stack)
+        stack
+    }
+    console.log('connected as id'+connection.threadId)
+
+    const user  = {name:'akshay',age:21}
+    const sql = 'insert into student set ?'
+
+    connection.query(sql,user,(err,result)=>{
+        if(err) throw err
+        console.log('1 record inserted id ',result.insertId)
+        connection.end()     
+    })
+})
---
+++ pathmodule.js
@@ -0,0 +1,8 @@
+const path  = require('path')
+// console.log(path.basename('C:\\temp\\myfile.html'))
+
+// console.log(process.env.PATH);
+
+
+// process.env.PATH.split(path.delimiter);
+// console.log(path.dirname('/240823062'))
