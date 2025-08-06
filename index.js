ca1ebef7a7930de19cb878f71b12c4a00d2ee90d
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-08-06 13:30:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+const express = require("express")
+const studentRoute = require("./routes/studentRoute")
+const app = express()
+const port = 80
+
+app.use(express.json())
+
+app.use("/student",studentRoute)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the home page');
+});
+
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}/`)
+})
