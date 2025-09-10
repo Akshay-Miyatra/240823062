f0262f6b0a6df5bf347eaa61776aadac06efd196
Akshay-Miyatra <mrakmiyatra@gmail.com>
2025-09-10 13:05:50 +0530

    new

--- Songs/index.js
+++ Songs/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 80;
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Miyatra_akshay:Akshay_29@cluster0.ujxun6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+ 
 
 app.use("/song", studentRoute);
 
